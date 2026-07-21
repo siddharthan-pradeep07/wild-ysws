6660980d559027c71145d4126bfbe7d41d6d2c4e
Siddharthan <siddharthansp07@gmail.com>
2026-07-21 18:18:40 +0530

    fixed email box alignment, fixed spanning of wavy divider,

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,38 @@ import WavyDivider from "@/components/WavyDivider";
 export default function HomePage() 
 {
   return (
-    <main className="min-h-screen bg-[#fff] flex flex-col items-center justify-start pt-0 px-10 text-center">
-      <div className="relative w-156 h-156 md:w-[920px] md:h-[520px] mb-2">
-        <Image
-          src="/logo_main_image-remove.png"
-          alt="Logo"
-          fill
-          className="object-contain"
-          priority
-        />
+    <main className="min-h-screen bg-[#fff] flex flex-col items-center justify-start pt-0 text-center">
+      <div className="w-full flex flex-col items-center px-10">
+        <div className="relative w-156 h-156 md:w-[920px] md:h-[520px] mb-2">
+          <Image
+            src="/logo_main_image-remove.png"
+            alt="Logo"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+
+        <h1 className="text-3xl md:text-5xl italic text-[#132A36] mb-4">
+          Ship something wild. 
+        </h1>
+
+        <form className="flex flex-col sm:flex-row gap-1 w-full max-w-sm mb-10 mx-auto justify-center">
+          <input
+            type="email"
+            required
+            placeholder="you@example.com"
+            className="input-email"
+          />
+          <button
+            type="submit"
+            className="btn-primary shrink-0"
+          >
+            get started
+          </button>
+        </form>
       </div>
 
-      <h1 className="text-3xl md:text-5xl italic text-[#132A36] mb-4">
-        Ship something wild. 
-      </h1>
-
-      <form className="flex flex-col sm:flex-row gap-1 w-full max-w-sm mb-10">
-        <input
-          type="email"
-          required
-          placeholder="you@example.com"
-          className="input-email"
-        />
-        <button
-          type="submit"
-          className="btn-primary"
-        >
-          get started
-        </button>
-      </form>
       <WavyDivider color="#132A36"/>
     </main>
   );
