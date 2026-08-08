3ab6a329cfd9781b1847f15d29c2081e6764b449
Siddharthan <siddharthansp07@gmail.com>
2026-08-08 13:50:52 +0530

    Added loading.tsx to display "loading" sign while the admin/shop sections are loading

---
+++ app/(dashboard)/admin/loading.tsx
@@ -0,0 +1,8 @@
+export default function AdminLoading()
+{
+    return (
+        <div className="text-center">
+            <p className="text-lg font-bold text-strong">Loading items...</p>
+        </div>
+    );
+}
---
+++ app/(dashboard)/shop/loading.tsx
@@ -0,0 +1,9 @@
+export default function ShopLoading()
+{
+    return (
+        <div className="text-center">
+            <p className="text-lg font-bold text-strong">Loading items...</p>
+        </div>
+    );
+}
+
